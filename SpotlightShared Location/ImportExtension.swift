a00571d68b17bbe921f60cd0c313ae343bb99fb0
Dr. Nathaniel Henry Fox (Scarlett Johansson) <deepgreengps@gmail.com>
2026-02-22 10:50:34 -0500

    Add files via upload

---
+++ SpotlightShared Location/ImportExtension.swift
@@ -0,0 +1,17 @@
+//
+//  ImportExtension.swift
+//  SpotlightShared Location
+//
+//  Created by Dr. Nathaniel Fox on 2/20/26.
+//
+
+import CoreSpotlight
+
+class ImportExtension: CSImportExtension {
+    
+    override func update(_ attributes: CSSearchableItemAttributeSet, forFileAt: URL) throws {
+        // Add attributes that describe the file at contentURL.
+        // Throw an error with details on failure.
+    }
+    
+}
